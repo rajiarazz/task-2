a5f8da2f73e75a5ef2967622d7aa3d4dffed2d71
rajiarazz <85434007+rajiarazz@users.noreply.github.com>
2021-06-08 16:48:06 +0530

    day 2

---
+++ day2/day2.py
@@ -0,0 +1,13 @@
+#1 ) Consider i = 2, Write a program to convert i to float.
+i=2
+print(float(i))
+
+#2 )Consider x="Hello" and y="World" , then write a program to concatinate the strings to a single string and print the result.
+x="Hello "
+y="World"
+print(x+y)
+
+#3 ) Consider pi = 3.14 . print the value of pie and its type.
+pi=3.14
+print(pi)
+type(pi)
